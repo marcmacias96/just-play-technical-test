e908a99de46f5f6a838a185a7b678f329952a114
Marco Macias <marc.macias96@hotmail.com>
2023-10-28 03:26:16 -0500

    Fix import

--- lib/features/city_sport_form/widgets/sports_grid.dart
+++ lib/features/city_sport_form/widgets/sports_grid.dart
@@ -2,7 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_play/assets/assets.dart';
-import 'package:just_play/features/city_sport_form/cubit/city_sport_form_cubit.dart';
+import 'package:just_play/features/city_sport_form/cubit/cubit.dart';
 import 'package:just_play/theme/theme.dart';
 import 'package:just_play/utils/alerts.dart';
 
